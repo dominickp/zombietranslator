cc7346628109717d00e7937652f1d2026784aa8c
Dominick Peluso <peluso.dominick@gmail.com>
2015-09-17 21:18:25 -0400

    Start building view

--- js/app/main.js
+++ js/app/main.js
@@ -2,16 +2,25 @@ require(['jquery'], function($) {
 
     $(document).ready(function () {
         $('#zombie-to-english-btn').click(function (event) {
-            unzombify();
+            unzombifyView();
             return false;
         });
 
         $('#english-to-zombie-btn').click(function (event) {
-            zombify();
+            zombifyView();
             return false;
         });
 
-        function zombify() {
+        function unzombifyView() {
+            $('#english').val(zombify($('#zombie').val()));
+            console.log('zombifyView');
+        }
+
+        function zombifyView() {
+            $('#zombie').val(unzombify($('#english').val()));
+        }
+
+        function zombify(english) {
             // 1. lower-case "r" at the end of words replaced with "rh".
             // 2. an "a" or "A" by itself will be replaced with "hra".
             // 3. the starts of sentences are capitalised (the "start of a sentence" is any occurrence of
@@ -21,15 +30,19 @@ require(['jquery'], function($) {
             // 6. "o" or "O" is replaced by "rrrRr"
             // 7. "u" or "U" is replaced by "rrrrRr"
             // 8. "r" or "R' is replaced by "RR"
-            $('#zombie').val($('#english').val());
+            //$('#zombie').val($('#english').val());
+            var zombie = english+'zombie';
+            return zombie;
         }
 
-        function unzombify() {
+        function unzombify(zombie) {
 
-
+            var english = zombie+'english';
+            return english;
         }
 
-        $('#english').on("keyup", zombify);
+        $('#english').on("keyup", zombifyView);
+        $('#zombie').on("keyup", unzombifyView);
 
     });
 
